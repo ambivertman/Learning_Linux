ebbe8b23fdedacb5b5cdcdfff237bb31dc66883b
ambi <forworkingliu@163.com>
2025-07-21 20:19:56 +0800

    一个项目多个.c文件逐个编译后链接成一个可执行程序

---
+++ Practice_Makefile/MakefileMode1/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "my_add.h"
+
+int main(void) {
+    int ret = my_sum(3, 4);
+    printf("ret = %d\n", ret);
+
+    return 0;
+}
---
+++ Practice_Makefile/MakefileMode1/my_add.h
@@ -0,0 +1,6 @@
+#ifndef __MY_ADD_H
+#define __MY_ADD_H
+#include "my_add.h"
+int my_sum(int num1, int num2);
+
+#endif
